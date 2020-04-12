7d5b958ae87b37a5e6eadb42e8abc27641e9d449
Mathieu_Simon <mathieu.simon1804@gmail.com>
2020-04-12 10:01:50 +0200

    first draft 2

--- project_modem_design.m
+++ project_modem_design.m
@@ -1,1 +1,16 @@
-ezf
+% LELEC2880: Modem design - Project
+% Authors: DE COCK Justin, DELHAYE Quentin, SIMON Mathieu
+% Date: 12/04/20
+
+%%  Step 1
+
+N=128; %number of subcarrier
+f_0=2E9; %carrier frequency
+f_sub=15E3; %carrier subspacing
+L=16; %cyclic prefix length
+
+% Es and N0 will have to vary but for now will let it like that 
+Es= 1; 
+N0= 1;
+
+
